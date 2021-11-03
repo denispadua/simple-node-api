f166f4656c4232a78290b2cb338e90a8d84e44c8
denispadua <denisdepadua@outlook.com>
2021-11-03 14:52:44 -0300

    arrumei a porta

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const port = process.env.port || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => { console.log('Our App Is Up And Running!'); });
+app.listen(process.env.PORT || 5000, () => { console.log('Our App Is Up And Running!'); });
 module.exports = app
